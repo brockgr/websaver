ff597cc251f2b6410369e6745ed54bf6e7314053
gavin@brock-family.org <gavin@brock-family.org@2e33bf54-4934-11de-898f-4722afa3338a>
2015-01-19 12:37:47 +0000

    Turn on WebGL by defualt

--- WebSaverView.m
+++ WebSaverView.m
@@ -122,6 +122,12 @@ static NSString * upArrow, *downArrow, *leftArrow, *rightArrow;
 		webView = [[WebView alloc] initWithFrame:frame];
 		[webView setDrawsBackground:NO];
         [webView setCustomUserAgent:@"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8) AppleWebKit/536.25 (KHTML, like Gecko) Version/6.0 Safari/536.25"];
+        
+        WebPreferences *preferences = [webView preferences];
+        if([preferences respondsToSelector:@selector(setWebGLEnabled:)]){
+            [preferences performSelector:@selector(setWebGLEnabled:) withObject:[NSNumber numberWithBool:YES]];
+        }
+
 
 		if (isPreview) {
 			[self scaleUnitSquareToSize:NSMakeSize( 0.25, 0.25 )];
@@ -250,7 +256,6 @@ static NSString * upArrow, *downArrow, *leftArrow, *rightArrow;
 		// DebugLog(@"time %f", [lastLoad timeIntervalSinceNow]);
 		if (reloadTimeFloat + [lastLoad timeIntervalSinceNow] < 0) {
 			[[webView mainFrame] reload];
-			[lastLoad dealloc];
 			lastLoad = [[NSDate alloc] init];
 			DebugLog(@"reloaded %@",[lastLoad description]);
 		}
